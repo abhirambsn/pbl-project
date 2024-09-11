0de1e83e0490754ff49d60ea33b6f8c89b6019cd
Bhallamudi Sai Narasimha Abhiram <abhirambsn@gmail.com>
2024-09-11 22:53:45 +0530

    Initialized Spring boot api

---
+++ profile-api/src/main/java/com/abhirambsn/profileapi/ProfileApiApplication.java
@@ -0,0 +1,13 @@
+package com.abhirambsn.profileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileApiApplication.class, args);
+    }
+
+}
---
+++ profile-api/src/test/java/com/abhirambsn/profileapi/ProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhirambsn.profileapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
