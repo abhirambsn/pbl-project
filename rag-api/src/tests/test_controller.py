71d736c4b6f4f8c9ffb6503010063bdc03660e4f
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-09-17 11:03:32 +0530

    Tests removed (#8)

--- rag-api/src/tests/test_controller.py
+++
@@ -1,8 +0,0 @@
-from ..controller import KnowledgeBaseController
-from ..repository import KnowledgeBaseRepository
-import pytest
-
-@pytest.mark.asyncio
-async def test_create_kb():
-    kbRepo = KnowledgeBaseRepository.KnowledgeBaseRepository()
-    response = await KnowledgeBaseController.create_kb()
