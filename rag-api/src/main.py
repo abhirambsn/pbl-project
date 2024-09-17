b1eb333cd14cce955a84bba419baccc344beb2c0
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-09-17 19:40:04 +0530

    Removed WS endpoint for rag-api

--- rag-api/src/main.py
+++ rag-api/src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .db import engine, Base
-from .controller import KnowledgeBaseController, WsController, ConfigController
+from .controller import KnowledgeBaseController, ConfigController
 from .middleware import LoggingMiddleware
 
 Base.metadata.create_all(bind=engine)
@@ -9,7 +9,6 @@ def create_app() -> FastAPI:
     app = FastAPI()
     app.add_middleware(LoggingMiddleware)
     app.include_router(ConfigController.router)
-    app.include_router(WsController.router)
     app.include_router(KnowledgeBaseController.router)
     return app
 
