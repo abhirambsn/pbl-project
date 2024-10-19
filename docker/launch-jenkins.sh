6dd5560c067ae2a597c0b780a5a86fa7c261d014
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-10-19 15:38:37 +0530

    Auth requirement change (#72)
    * Added Kubernetes Support
    
    * Auth requirement changed

--- docker/launch-jenkins.sh
+++ docker/launch-jenkins.sh
@@ -21,7 +21,7 @@ docker run \
     --env DOCKER_HOST=tcp://docker:2376 \
     --env DOCKER_CERT_PATH=/certs/client \
     --env DOCKER_TLS_VERIFY=1 \
-    --publish 10000:8080 \
+    --publish 10001:8080 \
     --publish 50000:50000 \
     --volume jenkins-data:/var/jenkins_home \
     --volume jenkins-docker-certs:/certs/client:ro ghcr.io/abhirambsn/pbl-jenkins-image:latest
