89b9a28a690b09c44054970835cd0f36659b3aac
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-10-19 17:49:14 +0530

    Secrets abstracted (#73)

---
+++ setup-secrets.sh
@@ -0,0 +1,15 @@
+#/bin/zsh
+
+# This script is used to setup the secrets for the project.
+# The script will prompt the user for the secrets and create k8s secrets for the project.
+
+SECRET_NAMES=("pinecone-api-key" "pg-db-url" "eureka-server")
+
+for SECRET_NAME in "${SECRET_NAMES[@]}"; do
+    read -sp "Enter value for secret [$SECRET_NAME]: " SECRET_VALUE
+    echo
+
+    kubectl create secret generic $SECRET_NAME --from-literal=$SECRET_NAME=$SECRET_VALUE
+
+    echo "Secret [$SECRET_NAME] created successfully."
+done
