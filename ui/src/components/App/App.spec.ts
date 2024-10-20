5ea805ce2b13538fe74398029ad9ab7e46e8da68
B.S.N <abhirambsn@gmail.com>
2024-10-20 15:49:02 +0530

    UI fix, and skaffold support

--- ui/src/components/App/App.spec.ts
+++ ui/src/components/App/App.spec.ts
@@ -1,12 +1,7 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import '@testing-library/jest-dom/vitest'
-describe("App", () => {
-    let useStateMock: any;
 
-    beforeEach(() => {
-        const setStateMock = vi.fn();
-        useStateMock = (useState: any) => [useState, setStateMock];
-    });
+describe("App", () => {
     it("expect 2+3 = 5", async () => {
         expect(2+3).toBe(5);
     })
