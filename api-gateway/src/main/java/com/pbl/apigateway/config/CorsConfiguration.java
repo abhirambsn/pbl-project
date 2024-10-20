93cef65cf62e2b7a533fa6d2c4b09b0c5c6a54cc
B.S.N <abhirambsn@gmail.com>
2024-10-20 18:00:53 +0530

    cors fix

--- api-gateway/src/main/java/com/pbl/apigateway/config/CorsConfiguration.java
+++ api-gateway/src/main/java/com/pbl/apigateway/config/CorsConfiguration.java
@@ -1,4 +1,21 @@
 package com.pbl.apigateway.config;
 
-public class CorsConfiguration {
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Component
+public class CorsConfiguration implements CorsConfigurationSource {
+    @Bean
+    public org.springframework.web.cors.CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(List.of("*"));
+        return configuration;
+    }
 }
