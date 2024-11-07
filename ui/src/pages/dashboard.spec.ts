c015efd6d46fdecfec48251452ca935a0872f90f
B.S.N <abhirambsn@gmail.com>
2024-11-07 08:43:20 +0530

    UI enahnce

---
+++ ui/src/pages/dashboard.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import '@testing-library/jest-dom/vitest'
+
+describe("Dashboard", () => {
+    it("expect 2+3 = 5", async () => {
+        expect(2+3).toBe(5);
+    })
+});
