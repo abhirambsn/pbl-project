0d71d65517d65e44e42915027148966e3fc03046
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-11-20 15:18:16 +0530

    added namespace (#97)

--- setup-ghcr-secret.sh
+++ setup-ghcr-secret.sh
@@ -6,4 +6,4 @@ read -p "Enter Email for Github Container Registry: " GH_EMAIL
 read -sp "Enter Github Token for Pull Access to Github Container Registry: " GH_TOKEN
 echo
 
-kubectl create secret docker-registry ghcr --docker-server=ghcr.io --docker-username=$GH_USERNAME --docker-password=$GH_TOKEN  --docker-email=$GH_EMAIL
+kubectl -n pbl create secret docker-registry ghcr --docker-server=ghcr.io --docker-username=$GH_USERNAME --docker-password=$GH_TOKEN  --docker-email=$GH_EMAIL
